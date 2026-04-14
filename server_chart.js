b131040b44b3e6ef1fb24205ed2886130fc806ce
vshalbw2345 <vshalbw234@gmail.com>
2026-04-14 15:45:43 +0530

    Add files via upload

---
+++ server_chart.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path    = require('path');
+const app     = express();
+const PORT    = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'chart.html')));
+app.listen(PORT, () => console.log(`Chart live on port ${PORT}`));
